62bb43a4eb319a2ca19db08cb747eda0bf2b79d0
Pedro Santos <90013568+PedroSantoss-Dev@users.noreply.github.com>
2022-11-23 21:41:47 -0300

    Feat: pagina de cadastro

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import logo from '../../assets/logo-dio.png';
+import { useNavigate } from 'react-router-dom';
 
 import { Button } from '../Button';
+
 
 import { Container, Wrapper, BuscarInputContainer, Input, Row, Menu, MenuRight, UserPicture} from './styles';
 
 const Header = ({autenticado}) => {
+  const navigate = useNavigate();
+
+ const cadastrar = () => {
+  navigate('/cadastro')
+ }
+  const Login = () => {
+    navigate('/login')
+  }
   return (
     <Wrapper>
       <Container>
@@ -27,8 +37,8 @@ const Header = ({autenticado}) => {
               ) : (
               <>
                 <MenuRight href="/">Home</MenuRight>
-                <Button title="Entrar" />
-                <Button title="Cadastrar" />
+                <Button onClick={Login} variant="primary" title="Entrar" />
+                <Button onClick={cadastrar} variant="primary" title="Cadastrar" />
               </>)}
           </Row>
       </Container>
